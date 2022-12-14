7272ced0f5e400bf8ab83b199231990cb64c0e8e
corentin-roussel <corentin.roussel@laplateforme.io>
2022-12-14 17:47:16 +0100

    Affichage du tableau complet et commencement de la requête sql pour recup les réservations

--- planning.php
+++ planning.php
@@ -1,20 +1,20 @@
 <?php
     include '_db/connect.php';
+ 
 
-    $currentDate = date('l d F Y', strtotime('last monday'));
-    for ($x=0 ; $x<7 ; $x++) {
-        echo '<th>' . $currentDate . '</th>' ;
-        $currentDate = date("l d F Y", strtotime($currentDate . ' +1 day'));
-    }    
+    $req = ("SELECT * FROM reservations INNER JOIN utilisateurs ON reservations.id_utilisateur = utilisateurs.id");
+    $req = mysqli_query($mysqli, $req);
+    $req = mysqli_fetch_all($req);
     
-    $currenthour = date('H:i', strtotime('8 hours 00 minutes'));
-    for ($x=0 ; $x<9 ; $x++) {
-        echo '<table><thead><tr><th>' . $currentHour . '</table></thead></tr></th>' ;
-        $currentHour = date("H:i", strtotime($currentHour . ' +1 hours'));
+    var_dump($req);
+
+
+    foreach($req as $key => $values) {
+        $hDebut = date("l d F Y H:i", strtotime($values[3]));
+        $hFin = date("l d F Y H:i", strtotime($values['4']));
+        var_dump($hDebut);
+        var_dump($hFin);
     }
-    
-    
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,20 +22,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Planning</title>
 </head>
 <body>
-    <table>
+    <table border="1">
         <thead>
             <tr>
+                <td>Réservations</td>
                 <?php   
-                        $currentDate = date('l d F Y', strtotime('last monday'));
-                        for ($x=0 ; $x<7 ; $x++) {
-                            echo '<th>' . $currentDate . '</th>' ;
-                            $currentDate = date("l d F Y", strtotime($currentDate . ' +1 day'));
-                        }     ?>
+                    $currentDate = date('l d F Y', strtotime('last monday'));
+                    for ($x=1 ; $x < 8 ; $x++) {
+                        echo '<th>' . $currentDate . '</th>' ;
+                        $currentDate = date("l d F Y", strtotime($currentDate . ' +1 day'));
+                    }?>
             </tr>
         </thead>
+        <tbody>
+            <?php
+                $currentHour = date('H:i', strtotime('8 am'));
+                for($j = 1; $j < 13; $j++) { 
+                    echo "<tr>";
+                    for($x = 1 ; $x < 8 ; $x++) {
+                        if($x == 1) { 
+                            echo '<td>' . $currentHour . '</td>' ;
+                            $currentHour = date("H:i", strtotime($currentHour . ' +1 hours'));
+                        }
+                        if($j == $x) {
+                            echo "<td>";
+                        }
+                        else {
+                            echo "<td>";
+                        }
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                }
+
+
+
+            ?>
+        </tbody>
     </table>
 </body>
 </html>
